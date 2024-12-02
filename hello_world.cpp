03510759d8f2a840c8d80a70b80dbfad40439a4e
zhuravlev0063 <zhuravlev0063@gmail.com>
2024-12-02 13:01:11 +0300

    Created hello world name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
 using namespace std;
+
 int main() {
     cout << "Hello, World!" << endl;
+    string name;
+    cout << "¬ведите ваше им€: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
     return 0;
 }
